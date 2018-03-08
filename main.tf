3cfd840d2d06c3791a9847c8d567e7966196ebba
Enrique Garbi <quique@enriquegarbi.com.ar>
2018-03-08 13:15:18 +0000

    extra dependency in DNS record

--- main.tf
+++ main.tf
@@ -171,6 +171,8 @@ module "task" {
 
 // Add ALB record on DNS
 resource "aws_route53_record" "main" {
+  depends_on = ["aws_ecs_service.main"]
+
   zone_id = "${var.zone_id}"
   name    = "${var.name}"
   type    = "A"
